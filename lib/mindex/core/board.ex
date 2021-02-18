ffaf121494fd3f79b8471ada15a3ad3c8e248987
Gio Lucas <giorgio.torres@sumup.com>
2021-02-18 10:40:39 -0300

    Board struct

---
+++ lib/mindex/core/board.ex
@@ -0,0 +1,12 @@
+defmodule Mindex.Core.Board do
+  defstruct answer: [], guesses: []
+
+  @spec new :: %Mindex.Core.Board{answer: list(), guesses: list()}
+  def new, do: %__MODULE__{answer: random_answer()}
+
+  @spec new(answer :: list()) :: %Mindex.Core.Board{answer: list(), guesses: list()}
+  def new([_a, _b, _c, _d] = answer), do: %__MODULE__{answer: answer}
+
+  @spec random_answer :: list
+  def random_answer, do: 1..8 |> Enum.shuffle() |> Enum.take(4)
+end
