10aee3b9a7ef22fd4732bccee4567500e24846d9
Gio Lucas <giorgio.torres@sumup.com>
2021-02-19 09:52:22 -0300

    Added multiplayer feature

--- lib/mindex.ex
+++ lib/mindex.ex
@@ -2,4 +2,12 @@ defmodule Mindex do
   @moduledoc """
   Documentation for `Mindex`.
   """
+  alias Mindex.Boundary.Server, as: Game
+
+  def add_game(name) when is_binary(name),
+    do: add_game(String.to_atom(name))
+
+  def add_game(name) do
+    DynamicSupervisor.start_child(MultiplayerServer, {Game, name})
+  end
 end
