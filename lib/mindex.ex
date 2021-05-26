c394a3db528d2052b8a88afaae5458b4c04744f6
Gio Lucas <torres.giorgio@gmail.com>
2021-05-26 11:01:02 -0300

    Removed dummy test

--- lib/mindex.ex
+++ lib/mindex.ex
@@ -2,17 +2,4 @@ defmodule Mindex do
   @moduledoc """
   Documentation for `Mindex`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mindex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/mindex_test.exs
+++ test/mindex_test.exs
@@ -1,8 +1,4 @@
 defmodule MindexTest do
   use ExUnit.Case
   doctest Mindex
-
-  test "greets the world" do
-    assert Mindex.hello() == :world
-  end
 end
